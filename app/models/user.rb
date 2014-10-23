b0050dda7aa354eb41742c7f702a44dfcfe96108
Jing <ladasa.tira@gmail.com>
2014-10-24 02:12:26 +0700

    Initial cancan ability done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+	     :recoverable, :rememberable, :trackable, :validatable
+
+    after_create :assign_default_role
+
+	def assign_default_role
+		add_role(:client) if self.roles.blank?
+	end
+
 end
