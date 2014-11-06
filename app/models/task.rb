d777915db9236a58f77db379b234da5cf995f4b2
Jing <ladasa.tira@gmail.com>
2014-11-07 01:25:34 +0700

    scope for task this month

--- app/models/task.rb
+++ app/models/task.rb
@@ -12,6 +12,13 @@ class Task < ActiveRecord::Base
   	scope :done, -> { where(completed: 't') }
   	scope :not_done, -> { where(completed: 'f') }
   	scope :now, -> { where(rank: '0', completed: 'f') }
+  	scope :this_month, -> { where("due_date >= ? AND due_date <= ?", 
+  Time.zone.now.beginning_of_month, Time.zone.now.end_of_month)}
 
+  	scope :this_week, -> { where("due_date >= ? AND due_date <= ?", 
+  Time.zone.now.beginning_of_week, Time.zone.now.end_of_week)}
+  	# def self.this_month
+  	# 	where("EXTRACT(MONTH FROM due_date)) = ?", Date.today.month )
+  	# end
 
 end
