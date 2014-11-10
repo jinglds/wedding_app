760768ff2ef7e4d6575ae05f86bc6103866e9adc
Jing <ladasa.tira@gmail.com>
2014-11-10 15:25:50 +0700

    add get shop api

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ WeddingApp::Application.routes.draw do
 
   root  'static_pages#home'
   namespace :api, defaults: {format: 'json'}  do
-    match "get_shop", to: "shops#get_shop", via: 'get'
+    match "get_shop", to: "shops#get_shop", via: 'post'
     resources :favorite_shops, only: [:create, :destroy, :unfav]
     resources :favorites, only: [:destroy]
     resources :shops  do   
