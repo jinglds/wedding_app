c5efa1cef6008b707a66daccad638fa8e621c1ea
Jing <ladasa.tira@gmail.com>
2014-12-04 02:02:41 +0700

    photo api

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -7,33 +7,46 @@ module Api
     	@photos = @shop.photos
   	end
 
-  	def create
+  	# def create
  
 
-  		@shop = Shop.find(params[:shop_id])
-    	# @photo = @shop.photos.build(photo_params)
+  	# 	@shop = Shop.find(params[:shop_id])
+   #  	# @photo = @shop.photos.build(photo_params)
 
-    	unless params[:photo][:images].nil?
-            params[:photo][:images].each do |a|
-            	# if @shop.photos << Photo.create(:image => a)
-        		# @shop.photos.create(:image => a)
-	      #   		render 'new'
-		   		# end
-		   		@photo = @shop.photos.build(:image => a)
-		   		unless  @photo.save
+   #  	unless params[:photo][:images].nil?
+   #          params[:photo][:images].each do |a|
+   #          	# if @shop.photos << Photo.create(:image => a)
+   #      		# @shop.photos.create(:image => a)
+	  #     #   		render 'new'
+		 #   		# end
+		 #   		@photo = @shop.photos.build(:image => a)
+		 #   		unless  @photo.save
 		   			
-	    			flash[:danger] = @photo.errors.full_messages.to_sentence
-		   		end
+	  #   			flash[:danger] = @photo.errors.full_messages.to_sentence
+		 #   		end
 
-        	end
-        	# redirect_to shop_photos_path
-        end
-        if no_cover?
-        	@shop.photos.first.update_attributes(:cover => true)
-        end
-    	return render :json=> {:success => true, :photos => @shop.photos}
+   #      	end
+   #      	# redirect_to shop_photos_path
+   #      end
+   #      if no_cover?
+   #      	@shop.photos.first.update_attributes(:cover => true)
+   #      end
+   #  	return render :json=> {:success => true, :photos => @shop.photos}
 
-  	end
-  	
+  	# end
+  	def create
+  		@shop = Shop.find(params[:shop_id])
+	    @photo = Photo.new(photo_params)
+	    if @photo.save
+	      render :json=> {:success => true, :photos => @photo.url}
+	    else
+	      render :json=> {:success => false, :photos => @photo.url}
+	    end
+	  end
+
+	  private
+	def photo_params
+	    params.require(:photo).permit(:image, :title)
+	end
   end
 end
---
+++ db/migrate/h.rb
@@ -0,0 +1,5 @@
+class DropShopTag < ActiveRecord::Migration
+  def change
+  	drop_table :table_name
+  end
+end
