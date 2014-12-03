29fa10423b3db7892d18f6bf78396beb197ea3dd
Jing <ladasa.tira@gmail.com>
2014-12-04 02:20:27 +0700

    photo api

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -34,16 +34,19 @@ module Api
    #  	return render :json=> {:success => true, :photos => @shop.photos}
 
   	# end
-  	def create
-  		@shop = Shop.find(params[:shop_id])
-	    @photo = Photo.new(photo_params)
-	    if @photo.save
-	      render :json=> {:success => true, :photos => "yay"}
-	    else
-	      render :json=> {:success => false, :photos => "nay"}
-	    end
-	  end
-
+  	# def create
+  	# 	@shop = Shop.find(params[:shop_id])
+	  #   @photo = Photo.new(photo_params)
+	  #   if @photo.save
+	  #     render :json=> {:success => true, :photos => "yay"}
+	  #   else
+	  #     render :json=> {:success => false, :photos => "nay"}
+	  #   end
+	  # end
+	 def create
+	  	photo = DataFile.save(params[:upload])
+	    render :text => "File has been uploaded successfully"
+	end
 	  private
 	def photo_params
 	    params.require(:photo).permit(:image, :title)
